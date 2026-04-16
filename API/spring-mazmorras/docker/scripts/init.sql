b218a61b1e367ef774ce1b59f1d280225b1c4472
Mel Varela <evsinsti@gmail.com>
2026-04-16 19:59:19 +0200

    Añadido el script de inicialización de la BBDD

--- API/spring-mazmorras/docker/scripts/init.sql
+++ API/spring-mazmorras/docker/scripts/init.sql
@@ -2,3 +2,89 @@
 CREATE DATABASE IF NOT EXISTS mazmorras;
 USE mazmorras;
 
+CREATE TABLE usuarios(
+    correo VARCHAR(100) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    contrasenna VARCHAR(200) NOT NULL,
+    foto_perfil VARCHAR(300)
+);
+
+CREATE TABLE campannas(
+    id VARCHAR(30) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    foto VARCHAR(300)
+);
+
+CREATE TABLE usuarios_campannas(
+    usuario VARCHAR(100),
+    campanna VARCHAR(30),
+    horario TEXT,
+    rol CHAR NOT NULL,
+    aceptada BOOLEAN NOT NULL,
+
+    PRIMARY KEY (usuario, campanna),
+
+    FOREIGN KEY (usuario) REFERENCES usuarios(correo),
+    FOREIGN KEY (campanna) REFERENCES campannas(id)
+);
+
+CREATE TABLE personajes(
+    id VARCHAR(30) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    clase VARCHAR(100),
+    subclase VARCHAR(100),
+    pg SMALLINT,
+    pgmaximos SMALLINT,
+    foto VARCHAR(300),
+    campanna VARCHAR(30),
+
+    FOREIGN KEY (campanna) REFERENCES campannas(id)
+);
+
+CREATE TABLE objetos(
+    id VARCHAR(30) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    precio FLOAT,
+    foto VARCHAR(300),
+    campanna VARCHAR(30),
+
+    FOREIGN KEY (campanna) REFERENCES campannas(id)
+);
+
+CREATE TABLE inventario(
+    personaje VARCHAR(30),
+    objeto VARCHAR(30),
+
+    PRIMARY KEY (personaje, objeto),
+
+    FOREIGN KEY (personaje) REFERENCES personajes(id),
+    FOREIGN KEY (objeto) REFERENCES objetos(id)
+);
+
+CREATE TABLE criaturas(
+    id VARCHAR(30) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    raza VARCHAR(100),
+    foto VARCHAR(300),
+    campanna VARCHAR(30),
+
+    FOREIGN KEY (campanna) REFERENCES campannas(id)
+);
+
+CREATE TABLE lugares(
+    id VARCHAR(30) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    foto VARCHAR(300),
+    campanna VARCHAR(30),
+
+    FOREIGN KEY (campanna) REFERENCES campannas(id)
+);
+
+CREATE TABLE notas(
+    id VARCHAR(30) PRIMARY KEY,
+    nombre VARCHAR(100) NOT NULL,
+    contenido TEXT,
+    dm BOOLEAN NOT NULL,
+    editando BOOLEAN NOT NULL,
+    propietario VARCHAR(30)
+);
