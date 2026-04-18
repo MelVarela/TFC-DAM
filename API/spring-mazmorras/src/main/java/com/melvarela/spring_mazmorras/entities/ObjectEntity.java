77af5050139e7833eeaf0e8031b3fa2bb3568beb
Mel Varela <evsinsti@gmail.com>
2026-04-18 19:26:53 +0200

    Añadidas las relaciones de JPA

--- API/spring-mazmorras/src/main/java/com/melvarela/spring_mazmorras/entities/ObjectEntity.java
+++ API/spring-mazmorras/src/main/java/com/melvarela/spring_mazmorras/entities/ObjectEntity.java
@@ -3,6 +3,8 @@ package com.melvarela.spring_mazmorras.entities;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -31,7 +33,8 @@ public class ObjectEntity {
     @Column(name = "foto")
     String picture;
 
-    @Column(name = "campanna")
-    String campaign;
+    @ManyToOne
+    @JoinColumn(name = "campanna")
+    CampaignEntity campaign;
 
 }
